3751b10f65ce18ee05ea47485ddc5ba035f79a4f
Pradnyads <sorepradnya@gmail.com>
2020-12-17 19:54:16 +0530

    [Pradnya]Refactor:For storing data Object is created

--- JS/employeePayRoll.js
+++ JS/employeePayRoll.js
@@ -1,38 +1,74 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  const name = document.querySelector('#name');
-  const textError = document.querySelector('.text-error');
-  name.addEventListener('input', function() {
-      if(name.value.length == 0) {
-        textError.textContent = "";
-        return;
-      }
-      try {
-        (new EmployeePayrollData).name = name.value;;
-        textError.textContent = "";
-      } catch (e) {
-        textError.textContent = e;
-   }
+window.addEventListener("DOMContentLoaded", (event) => {
+  const name = document.querySelector("#name");
+  const textError = document.querySelector(".text-error");
+  name.addEventListener("input", function () {
+    if (name.value.length == 0) {
+      textError.textContent = "";
+      return;
+    }
+    try {
+      new EmployeePayrollData().name = name.value;
+      textError.textContent = "";
+    } catch (e) {
+      textError.textContent = e;
+    }
   });
-  window.addEventListener('DOMContentLoaded', (event) => {
-    const startDate = document.querySelector('#startDate');
-    const dateError = document.querySelector('.date-error');
-    startDate.addEventListener('input', function() {
-       if (startDate.value.length == 0) {
-          dateError.textContent = "";
-          return;
-       }
-       try {
-           (new EmployeePayrollData).startDate = startDate.value;;
-           dateError.textContent = "";
-       } catch (e) {
-       dateError.textContent = e;
-      }
-    });
-   const salary = document.querySelector("#salary");
-   const output = document.querySelector(".salary-output");
-   output.textContent = salary.value;
-   salary.addEventListener("input", function () {
-   output.textContent = salary.value;
+  const salary = document.querySelector("#salary");
+  const output = document.querySelector(".salary-output");
+  output.textContent = salary.value;
+  salary.addEventListener("input", function() {
+    output.textContent = salary.value;
   });
- });
 });
+
+const save = () => {
+  try {
+    let employeePayrollData = createEmployeePayroll();
+  } catch (e) {
+    return;
+  }
+};
+
+const createEmpoyeePayroll = () => {
+  let employeePayrollData = new EmployeePayrollDate();
+  try {
+    employeePayrollData.name = getInputValueById("#name");
+  } catch (e) {
+    setTextValue(".text-error", e);
+    throw e;
+  }
+
+  employeePayrollData.profilePic = getSelectedValues("[name=profile]").pop();
+  employeePayrollData.gender = getSelectedValues("[name=gender]").pop();
+  employeePayrollData.department = getSelectedValues("[name=department]");
+  employeePayrollData.salary = getInputValueById("#salary");
+  employeePayrollData.note = getInputValueById("#notes");
+  let date =
+    getInputValueById("#day") +
+    " " +
+    getInputValueById("#month") +
+    " " +
+    getInputValueById("#year");
+  employeePayrollData.date = Date.parse(date);
+  alert(employeePayrollData.toString());
+  return employeePayrollData;
+};
+
+const getSelectedValues = (propertyValue) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  let selItems = [];
+  allItems.forEach((item) => {
+    if (item.checked) selItems.push(item.value);
+  });
+  return selItems;
+};
+/*
+ *1: querySelector is newer feature.
+ *2: the querySelector method can be used when selecting by element name, nesting, or class name
+ *3: querySelector lets you find elements with rules that cant be expressed with getElementById
+ */
+const getInputValueById = (id) => {
+  let value = document.querySelector(id).value;
+  return value;
+};
+
